93e0747db483091cfd3dcf0b98b5aa91a3daa4b5
bruno_gaddini <bruno_gaddini@hotmail.com>
2023-04-02 01:01:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Our main file
+
+import speech_recognition as sr
+
+# Create a recognizer
+r = sr.Recognizer()
+
+# Open the mic for capture
+with sr.Microphone() as source:
+    audio = r.listen(source) # Defines the audio source
+
+    print(r.recognize_google(audio))
